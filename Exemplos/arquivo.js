3c1a3059a0ebd4890d5d80b31ced21926f38b0c6
Ferbatman <fernandohsilvestre@hotmail.com>
2023-02-15 00:39:26 -0300

    Aula 14/02

---
+++ Exemplos/arquivo.js
@@ -0,0 +1,21 @@
+// alert("Olá Sala!");
+
+// document.write("Olá!");
+
+// let x = confirm("oi");
+// console.log(x);
+
+// let nome = prompt("Digite seu nome","");
+// if(confirm("Quer ver seu nome?")){
+//     alert("seu nome é: "+ nome);
+// }
+
+// let i;
+// for(i=1; i<=6; i++){
+//     document.write("<h"+i+">Olá sala</h"+i+">");
+// }
+
+//1)
+
+
+
